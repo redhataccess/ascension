1133b773a6d129ec7b726edbed1892f229ad540d
Samuel Mendenhall <Samuel.Mendenhall@gmail.com>
2015-01-27 12:42:38 -0500

    Showing sbrs and tags in the case header, reworking the case header as well

--- public/js/react_components/models/case/caseHeader.jsx
+++ public/js/react_components/models/case/caseHeader.jsx
@@ -1,11 +1,17 @@
 var React       = require('react');
 var S           = require('string');
 var CaseProduct = require('./caseProduct.jsx');
+var CaseSbrs    = require('./caseSbrs.jsx');
+var CaseTags    = require('./caseTags.jsx');
 var TaskDates   = require('../task/taskDates.jsx');
 var Spacer      = require('react-redhat/Spacer');
 var ResourceOpEnum    = require('../../../../../src/com/redhat/ascension/rules/enums/ResourceOpEnum.coffee');
 
 var Well            = require('react-bootstrap/Well');
+var Grid            = require('react-bootstrap/Grid');
+var Row             = require('react-bootstrap/Row');
+var Label           = require('react-bootstrap/Label');
+var Col             = require('react-bootstrap/Col');
 var OverlayTrigger  = require('react-bootstrap/OverlayTrigger');
 var Tooltip         = require('react-bootstrap/Tooltip');
 
@@ -21,35 +27,47 @@ var Component = React.createClass({
         } 
         return null;
     },
+    genFtsLabel: function(c) {
+        if (c.resource.isFTSCase == true) {
+            return <span>&nbsp;<Label bsStyle="danger">FTS</Label></span>;
+        }
+        return null;
+    },
+    genTamLabel: function(c) {
+        if (c.resource.isTAMCase == true) {
+            return <span>&nbsp;<Label bsStyle="primary">TAM</Label></span>;
+        }
+        return null;
+    },
     render: function() {
         var caseNumber = S(this.props.case.resource.caseNumber).padLeft(8, '0').s;
         return (
             <Well key='caseHeader'>
-                <div>
-                    <div className='pull-left'>
+                <Row>
+                    <Col md={6}>
                         <h2 key='header'>
                             {this.genEntityOpText(this.props.case)} 
                             Case {caseNumber}
+                            {this.genFtsLabel(this.props.case)}
+                            {this.genTamLabel(this.props.case)}
                         </h2>
-                    </div>
-                    <div className='pull-right'>
+                        <div className='clearfix'></div>
+                        <span>{this.props.case.resource.status} / {this.props.case.resource.internalStatus}</span>
+                        &nbsp;--&nbsp;
+                        <span>{this.props.case.resource.severity} / {this.props.case.resource.internalPriority}</span>
+                        <div className='clearfix'></div>
+                        <TaskDates task={this.props.case} key='dates'></TaskDates>
+                    </Col>
+                    <Col md={6}>
                         <CaseProduct case={this.props.case.resource} key='product'></CaseProduct>
-                    </div>
-                </div>
-                <div className='clearfix'></div>
-                <span>{this.props.case.resource.status} / {this.props.case.resource.internalStatus}</span>
-                &nbsp;--&nbsp;
-                <span>{this.props.case.resource.severity} / {this.props.case.resource.internalPriority}</span>
-                <div className='clearfix'></div>
-                <TaskDates task={this.props.case} key='dates'></TaskDates>
-                <Spacer />
-                <strong key='headerSubject'>{this.props.case.resource.subject}</strong>
-                <Spacer />
-                {/*/////////////////////////////////////////////////////////////////////////////////*/}
-                {/*Case link buttons */}
-                {/*/////////////////////////////////////////////////////////////////////////////////*/}
-                <div className="row">
-                    <div className='col-sm-6'>
+                        <Spacer />
+                        <CaseSbrs case={this.props.case} key='sbrs'></CaseSbrs>
+                        <Spacer />
+                        <CaseTags case={this.props.case} key='tags'></CaseTags>
+                        <Spacer />
+                        {/*/////////////////////////////////////////////////////////////////////////////////*/}
+                        {/*Case link buttons */}
+                        {/*/////////////////////////////////////////////////////////////////////////////////*/}
                         <div key='caseLinks'>
                             <a className='btn btn-open' target='_blank' href={`https://na7.salesforce.com/${this.props.case.externalModelId}`}>
                                 {`Salesforce`}
@@ -59,17 +77,11 @@ var Component = React.createClass({
                                 {`Unified`}
                             </a>
                         </div>
-                    </div>
-                    {/*
-                    <div className='col-sm-6'>
-                        <div className='pull-right'>
-                            <OverlayTrigger placement="top" overlay={<Tooltip>Ignore case until next time it is updated.</Tooltip>}>
-                                <button className='btn btn-secondary' onClick={this._declineTask}>{`Ignore`}</button>
-                            </OverlayTrigger>
-                        </div>
-                    </div>
-                    */}
-                </div>
+                    </Col>
+                </Row>
+                <Spacer />
+                <strong key='headerSubject'>{this.props.case.resource.subject}</strong>
+                <Spacer />
             </Well>
         )
     }
